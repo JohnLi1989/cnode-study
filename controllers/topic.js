0e754779acd6dfde6285dbe86a4345bcfdfbb057
JohnLi1989 <lj85046673@hotmail.com>
2016-04-13 01:38:03 +0800

    add publish view

--- controllers/topic.js
+++ controllers/topic.js
@@ -1,6 +1,14 @@
 /**
  * Created by john on 16/4/12.
  */
+var validator = require('validator');
+
 exports.showCreate = function(req,res){
     res.render('create');
-}
+};
+
+exports.create = function(req,res){
+    var title = validator.trim(req.body.title);
+    var tab = validator.trim(req.body.tab);
+    var content = validator.trim(req.body.t_content);
+};
